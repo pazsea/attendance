b8924fee0d0e5e0d63a974c870e31309889d0d7e
Patrick Sjöberg <43534461+pazsea@users.noreply.github.com>
2019-10-31 08:58:57 +0100

    15 16 admin will see attendance at current date (#16)
    * changed context state to store
    
    * leaving classattanedance
    
    * new way to calculate valid date
    
    * merge with master
    
    * styles included in myAttendance
    
    * one way to go
    
    * sorting works in AdminClassAttendance
    
    * firebase implemented

--- src/components/AdminClassAttendance/index.js
+++ src/components/AdminClassAttendance/index.js
@@ -4,92 +4,295 @@ import {
   SCAdminClassContainer,
   SCArrowLeftIcon,
   SCArrowRightIcon,
-  SCArrowDownIcon
+  SCArrowDownIcon,
+  SCSPanButton
 } from "./styles";
+
+import Loading from "../Loading";
 
 import Button from "@material-ui/core/Button";
 import Switch from "@material-ui/core/Switch";
 import firebase from "../Firebase";
 
+// Ändra så att switchen är om studenten är false, Även Button färgen.
+// Gör funktion när switchen dras.
+// Fixa sorteringen...sadasd
+
 const AdminClassAttendance = () => {
   const {
-    classDetailsObject: [{ selectedClass }]
+    classDetailsObject: [{ selectedClassUid, className }]
   } = useContext(Context);
+
+  const [noLectureState, setNoLectureState] = useState(false);
+  const [selectedLecture, setSelectedLecture] = useState(null);
+  const [filteredAttendanceState, setFilterAttendanceState] = useState({
+    filteredAttendanceStateStudents: null,
+    abscentFiltered: false,
+    presentFiltered: false
+  });
+
+  const [lectureIndex, setLectureIndex] = useState({
+    maxIndex: null,
+    currentIndex: null
+  });
+
+  const [allLectures, setAllLectures] = useState(null);
 
   const [navigationState, setNavigationState] = useState(false);
 
+  // useEffect(() => {
+  //   console.log(lectureIndex);
+  // }, [lectureIndex]);
+
   useEffect(() => {
+    if (allLectures) {
+      console.log("Set Class to closest to your date");
+      setLectureIndex({
+        maxIndex: Number(allLectures.length - 1),
+        currentIndex: Number(allLectures.length - 1)
+      });
+      setSelectedLecture(allLectures[Number(allLectures.length - 1)]);
+
+      // setLectureState(prevState => ({
+      //   ...prevState,
+      //   selectedLectureIndex: prevState.allLectures.length - 1
+      // }));
+    }
+  }, [allLectures]);
+
+  useEffect(() => {
+    console.log("startar");
     const unsubscribe = firebase
-      .classDetails(selectedClass)
+      .classDetails(selectedClassUid)
       .collection("attendance")
-      // .orderBy("timestamp")
       .onSnapshot(snap => {
-        let orderDocs = snap.docs.sort(function(a, b) {
-          return Number(a.id) - Number(b.id);
-        });
-        let result = orderDocs.reduce((acc, doc) => {
-          return [
-            ...acc,
-            {
-              timeStamp: doc.id,
-              date: [Date.now(doc.id)],
-              students: doc.data()
-            }
-          ];
-        }, []);
-        // let result = snap.docs.map(doc => doc);
-        console.log(result);
+        if (snap.empty) {
+          setNoLectureState(true);
+        } else {
+          let orderDocs = snap.docs.sort(function(a, b) {
+            return Number(a.id) - Number(b.id);
+          });
+
+          let orderedDocsToState = orderDocs.reduce((acc, obj) => {
+            return [
+              ...acc,
+              {
+                className: className,
+                lectureTimeUid: obj.id,
+                date: new Date(Number(obj.id) + 10000000)
+                  .toISOString()
+                  .slice(0, 10),
+                students: obj.data()
+                  ? Object.keys(obj.data()).map(name => ({
+                      name: name,
+                      attendance: obj.data()[name]
+                    }))
+                  : null
+              }
+            ];
+          }, []);
+          setAllLectures(orderedDocsToState);
+        }
       });
 
     return () => {
       unsubscribe();
     };
-  }, [selectedClass]);
+  }, [selectedClassUid, className]);
+
+  const changeLecture = value => {
+    const { maxIndex, currentIndex } = lectureIndex;
+
+    if (value === "increment" && maxIndex === currentIndex) {
+      setLectureIndex(prevState => ({
+        ...prevState,
+        currentIndex: 0
+      }));
+      setSelectedLecture(allLectures[0]);
+    } else if (value === "increment") {
+      setLectureIndex(prevState => ({
+        ...prevState,
+        currentIndex: Number(currentIndex + 1)
+      }));
+      setSelectedLecture(allLectures[Number(currentIndex + 1)]);
+    } else if (value === "decrement" && currentIndex === 0) {
+      setLectureIndex(prevState => ({
+        ...prevState,
+        currentIndex: maxIndex
+      }));
+      setSelectedLecture(allLectures[maxIndex]);
+    } else {
+      setLectureIndex(prevState => ({
+        ...prevState,
+        currentIndex: currentIndex - 1
+      }));
+      setSelectedLecture(allLectures[currentIndex - 1]);
+    }
+  };
+
+  const changeStudentAttendance = (studentName, studentAttendance) => {
+    const { lectureTimeUid } = selectedLecture;
+
+    let path = firebase
+      .classDetails(selectedClassUid)
+      .collection("attendance")
+      .doc(lectureTimeUid);
+
+    firebase.db.runTransaction(transaction => {
+      return transaction.get(path).then(lectureDoc => {
+        if (lectureDoc[studentName] === studentAttendance) {
+          return;
+        } else {
+          transaction.update(path, {
+            [studentName]: !studentAttendance
+          });
+        }
+      });
+    });
+  };
+
+  const filterFalse = () => {
+    if (allLectures) {
+      if (filteredAttendanceState.abscentFiltered) {
+        setFilterAttendanceState(prevState => ({
+          ...prevState,
+          filteredStudents: null,
+          abscentFiltered: false,
+          presentFiltered: false
+        }));
+      } else {
+        const filterAttendingFalse = selectedLecture.students.filter(function(
+          student
+        ) {
+          return student.attendance === false;
+        });
+        setFilterAttendanceState(prevState => ({
+          ...prevState,
+          filteredStudents: filterAttendingFalse,
+          abscentFiltered: true,
+          presentFiltered: false
+        }));
+      }
+    }
+  };
+
+  const filterTrue = () => {
+    if (allLectures) {
+      if (filteredAttendanceState.presentFiltered) {
+        setFilterAttendanceState(prevState => ({
+          ...prevState,
+          filteredStudents: null,
+          presentFiltered: false,
+          abscentFiltered: false
+        }));
+      } else {
+        const filterAttendingTrue = selectedLecture.students.filter(function(
+          student
+        ) {
+          return student.attendance === true;
+        });
+        setFilterAttendanceState(prevState => ({
+          ...prevState,
+          filteredStudents: filterAttendingTrue,
+          presentFiltered: true,
+          abscentFiltered: false
+        }));
+      }
+    }
+  };
+
+  const { filteredStudents } = filteredAttendanceState;
 
   return (
-    <SCAdminClassContainer navigationState={navigationState}>
-      <div className="adminClassInfo">
-        <SCArrowLeftIcon></SCArrowLeftIcon>
-        <span>
-          <h1>26 MAJ 2019</h1>
-          <p>FE16 närvarostatus</p>
-        </span>
-        <SCArrowRightIcon></SCArrowRightIcon>
-      </div>
-      <div
-        className="adminClassNav"
-        onClick={() => setNavigationState(!navigationState)}
-      >
-        <p>
-          Nurvarande sortering <SCArrowDownIcon></SCArrowDownIcon>
-        </p>
-        <div className="sortingSelections">
-          <button>Allas närvaro</button>
-          <button>Frånvarande</button>
-          <button>Närvarande</button>
-        </div>
-      </div>
-      <div className="adminClassStudents">
-        <Button
-          className="submitButton"
-          variant="contained"
-          color="primary"
-          type="button"
-        >
-          Patrick Anders
-          <Switch size="small"></Switch>
-        </Button>
-        <Button
-          className="submitButton"
-          variant="contained"
-          color="primary"
-          type="button"
-        >
-          hello
-          <Switch></Switch>
-        </Button>
-      </div>
-    </SCAdminClassContainer>
+    <>
+      {noLectureState ? (
+        <div>Denna klass har ingen anmäld närvaro än...</div>
+      ) : selectedLecture ? (
+        <SCAdminClassContainer navigationState={navigationState}>
+          <div className="adminClassInfo">
+            <SCArrowLeftIcon
+              onClick={() => changeLecture("decrement")}
+            ></SCArrowLeftIcon>
+            <span>
+              <h1>{selectedLecture.date}</h1>
+              <p>{selectedLecture.className} närvarostatus</p>
+            </span>
+            <SCArrowRightIcon
+              onClick={() => changeLecture("increment")}
+            ></SCArrowRightIcon>
+          </div>
+          <div
+            className="adminClassNav"
+            onClick={() => setNavigationState(!navigationState)}
+          >
+            <p>
+              Sortera <SCArrowDownIcon></SCArrowDownIcon>
+            </p>
+            <div className="sortingSelections">
+              <button onClick={filterFalse}>Frånvarande</button>
+              <button onClick={filterTrue}>Närvarande</button>
+            </div>
+          </div>
+          <div className="adminClassStudents">
+            {filteredStudents
+              ? filteredStudents.map((student, index) => (
+                  <SCSPanButton
+                    key={student.name + "span" + index}
+                    attending={student.attendance}
+                  >
+                    <Button
+                      key={student.name + index}
+                      disableTouchRipple
+                      className="submitButton"
+                      variant="contained"
+                      type="button"
+                    >
+                      {student.name}
+                      <Switch
+                        color="primary"
+                        onChange={() =>
+                          changeStudentAttendance(
+                            student.name,
+                            student.attendance
+                          )
+                        }
+                        defaultChecked={student.attendance}
+                      ></Switch>
+                    </Button>
+                  </SCSPanButton>
+                ))
+              : selectedLecture.students.map((student, index) => (
+                  <SCSPanButton
+                    key={student.name + "span" + index}
+                    attending={student.attendance}
+                  >
+                    <Button
+                      key={student.name + index}
+                      disableTouchRipple
+                      className="submitButton"
+                      variant="contained"
+                      type="button"
+                    >
+                      {student.name}
+                      <Switch
+                        color="primary"
+                        onChange={() =>
+                          changeStudentAttendance(
+                            student.name,
+                            student.attendance
+                          )
+                        }
+                        defaultChecked={student.attendance}
+                      ></Switch>
+                    </Button>
+                  </SCSPanButton>
+                ))}
+          </div>
+        </SCAdminClassContainer>
+      ) : (
+        <Loading text="Hämtar föreläsningar..."></Loading>
+      )}
+    </>
   );
 };
 
