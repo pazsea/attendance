e3558f75e6dcc33c10427aeda34a966ccbfd434c
glindberg <gustav.lindberg1@gmail.com>
2019-11-01 14:23:49 +0100

    Saving work before merging new master into branch

--- src/components/AdminExport/index.js
+++ src/components/AdminExport/index.js
@@ -1,7 +1,41 @@
-import React from "react";
+// import React from "react";
 
-const AdminExport = () => {
-  return <div>EXPORT NÄRVARO</div>;
+// const AdminExport = () => {
+//   return <div>EXPORT NÄRVARO</div>;
+// };
+
+// export default AdminExport;
+
+// ----  { Libraries } ----
+import React from "react";
+import { SCInfoComponentContainer } from "./styles";
+import "./adminexport.scss";
+import { makeStyles } from "@material-ui/core/styles";
+// ----  { Routes, ActionTypes etc. Custom variables. } ----
+// ----  { Styles } ----
+
+// ----  { Backend } ----
+// ----  { Render Components } -----
+
+// const useStyles = makeStyles({
+//   root: {
+//     flexGrow: 1
+//   }
+// });
+
+const AdminExport = ({ text }) => {
+  // const classes = useStyles();
+  return (
+    <SCInfoComponentContainer>
+      {/* <h3>Inga klasser hittades</h3> */}
+      <h3>Inga föreläsningar hittdes för valt datum</h3>
+
+      <p>{text}....</p>
+    </SCInfoComponentContainer>
+  );
 };
 
 export default AdminExport;
+
+//Inga klasser finns registrerade.
+// Inga föreläsningar finns registrerade
---
+++ src/components/AdminExport/styles.js
@@ -0,0 +1,5 @@
+import styled from "styled-components/macro";
+
+export const SCInfoComponentContainer = styled.div`
+  border: 2 px solid orange;
+`;
